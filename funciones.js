6ce5f43614c16a213f64e4fd25ce171f0b7dcc8b
Pablo N. Rodriguez Muriedas <xe1tkt@hotmail.com>
2023-10-24 12:09:22 -0700

    v9 curso

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function  obtenerDatosHtml(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+function obtenerContactos()
+{
+    let hoja=SpreadsheetApp.openById('1okPSJ-S5R7wNfW_No6E_Fq9fQYFQNaNt7OAjBmZIhoQ').getActiveSheet();
+    let datos= hoja.getDataRange().getValues();
+    return datos;
+
+}

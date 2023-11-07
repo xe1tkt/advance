971ea38e105437f5eeb1cf3e023a16c1d7fb8364
Pablo N. Rodriguez Muriedas <xe1tkt@hotmail.com>
2023-11-07 08:49:22 -0700

    v10 curso

--- funciones.js
+++ funciones.js
@@ -1,3 +1,5 @@
+
+const HOJA =SpreadsheetApp.openById('1okPSJ-S5R7wNfW_No6E_Fq9fQYFQNaNt7OAjBmZIhoQ').getActiveSheet();
 function doGet()
 {
 return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Gooogle Apps Script');
@@ -9,8 +11,21 @@ function  obtenerDatosHtml(nombre)
 }
 function obtenerContactos()
 {
-    let hoja=SpreadsheetApp.openById('1okPSJ-S5R7wNfW_No6E_Fq9fQYFQNaNt7OAjBmZIhoQ').getActiveSheet();
-    let datos= hoja.getDataRange().getValues();
+  
+    let datos= HOJA.getDataRange().getValues();
     return datos;
 
+}
+function importarContactos()
+{
+    let url='https://randomuser.me/api/?results=5&inc=name,email,phone';
+    respuesta=UrlFetchApp.fetch(url).getContentText();
+    let datos=JSON.parse(respuesta);
+   datos.results.forEach(insertarContactoJson);
+   //datos.results.forEach(contacto=>Logger.log(contacto));
+
+}
+function insertarContactoJson(contacto)
+{
+    HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email,contacto.phone]);
 }
